c6c2afa42fe2894a4edd877ebf3337c064f580b6
samratcirisanagandla <cirisanagandla.s@mobiusdtaas.ai>
2024-10-11 12:48:05 +0530

    Update notify_google_chat.sh

--- notify_google_chat.sh
+++ notify_google_chat.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-SERVICE_CODE="${SERVICE_CODE:-mobius-utility-service}"
-WORKFLOW_STATUS="$2"
-WEBHOOK_URL="${WEBHOOK_URL:-https://chat.googleapis.com/v1/spaces/AAAAbkkpaIU/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=yYYoov7obNuUq86dW5E7RkHreUpcoROju45s_q7EIpM}"
-ACTION_URL="${ACTION_URL:-https://github.com/my-repo/actions/runs/12345}"
+#SERVICE_CODE="${SERVICE_CODE:-mobius-utility-service}"
+#WORKFLOW_STATUS="$2"
+#WEBHOOK_URL="${WEBHOOK_URL:-https://chat.googleapis.com/v1/spaces/AAAAbkkpaIU/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=yYYoov7obNuUq86dW5E7RkHreUpcoROju45s_q7EIpM}"
+#ACTION_URL="${ACTION_URL:-https://github.com/my-repo/actions/runs/12345}"
+
+SERVICE_CODE=$SERVICE_CODE
+WORKFLOW_STATUS=$WORKFLOW_STATUS
+WEBHOOK_URL=$WEBHOOK_URL
+ACTION_URL=$ACTION_URL
+
 
 if [ "$WORKFLOW_STATUS" == "success" ]; then
 	MESSAGE="Workflow completed successfully for service: $SERVICE_CODE. [View Details]($ACTION_URL)"
